60520f7ba9ca8e3f444aa546a856db85390c4454
Boney Patel <boneypatel37@yahoo.com>
2024-04-20 22:20:27 -0400

    Add tmux configs and vim-test

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,5 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.o.expandtab = true
 vim.o.relativenumber = true
+local set = vim.opt -- set options
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 return {}
